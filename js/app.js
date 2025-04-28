a90f2e88229d50ae2539c82f356b1d4856f1e368
BorealAce <gabrielborgesgs20@gmail.com>
2025-04-28 16:16:19 -0300

    Funções limpar + adicionar completas

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
+let elemCarrinho = document.getElementById('lista-produtos');
+let elemValorTotal = document.getElementById('valor-total');
+let valorTotal = 0;
+
 function adicionar() {
-    let elemProduto = document.getElementById("produto").value.split(" - R$");
+    let elemProduto = document.getElementById('produto').value.split(' - R$');
     let qtd = parseInt(document.getElementById('quantidade').value);
     let descProduto = elemProduto[0];
-    let preco = parseInt(elemProduto[1]);
-    let precoSubTotal = quantidade * preco
-    console.log(preco); 
-    let elemCarrinho = document.getElementById('lista-produtos');
+    let preco = parseFloat(elemProduto[1]);
+    let precoSubTotal = qtd * preco;
 
     if (qtd > 0) {
         elemCarrinho.innerHTML += `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${qtd}x</span> ${descProduto} <span class="texto-azul">R$${precoSubTotal}</span>
+          <span class="texto-azul">${qtd}x</span> ${descProduto} <span class="texto-azul">R$${precoSubTotal.toFixed(2)}</span>
         </section>`;
-    
-    
+        
+        valorTotal += precoSubTotal;
+        elemValorTotal.textContent = `R$${valorTotal.toFixed(2)}`;
     }
 } 
+
+function limpar() {
+    while (elemCarrinho.firstChild) {
+        elemCarrinho.removeChild(elemCarrinho.firstChild);
+    }
+    valorTotal = 0;
+    elemValorTotal.textContent = `R$${valorTotal.toFixed(2)}`;
+    quantidade.value = '';
+}
